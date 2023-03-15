fe881dd858331fbbcfe27263ab4eadff3cd5e469
DaBeIDS <90916810+DaBeIDS@users.noreply.github.com>
2023-03-15 10:09:41 +0100

    Update entry.sh
    Signed-off-by: DaBeIDS <90916810+DaBeIDS@users.noreply.github.com>

--- data_extractor/code/rule_based_pipeline/entry.sh
+++ data_extractor/code/rule_based_pipeline/entry.sh
@@ -1,8 +1,3 @@
 #!/bin/bash
 
-date_time=$(date +'%d_%h_%Y:%H_%M')
-log_file_path="/app/server_logs/rule_based_logs_${date_time}.txt"
-cd /app/code/rule_based_pipeline/rule_based_pipeline
-python3 rb_server.py > $log_file_path 2>&1 &
-
 sleep infinity
