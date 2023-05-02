0213a4d7d19f3b84eeadd91d0ec5d78b4c9b5958
DaBeIDS <90916810+DaBeIDS@users.noreply.github.com>
2023-05-02 15:58:07 +0200

    Update entry.sh
    Signed-off-by: DaBeIDS <90916810+DaBeIDS@users.noreply.github.com>
    Signed-off-by: VEIY82L <david.besslich@investmentdataservices.com>

--- data_extractor/code/esg_data_pipeline/entry.sh
+++ data_extractor/code/esg_data_pipeline/entry.sh
@@ -1,11 +1,3 @@
 #!/bin/bash
 
-date_time=$(date +'%d_%h_%Y:%H_%M')
-log_file_path="/app/server_logs/extractions_logs_${date_time}.txt"
-cd /app/code/esg_data_pipeline/esg_data_pipeline
-python3 extraction_server.py > $log_file_path 2>&1 &
-
-#cd /esg_data_pipeline/notebooks
-#jupyter notebook --ip=0.0.0.0 --port=8888 --no-browser --allow-root --NotebookApp.token='' --NotebookApp.password=''
-
 sleep infinity
