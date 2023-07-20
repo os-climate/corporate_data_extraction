2b75604ce96ce65ba1179c30c40c1701557028aa
DaBeIDS <90916810+DaBeIDS@users.noreply.github.com>
2023-07-20 08:58:42 +0200

    Update inference_server.py
    Undo previous push (wrong branch)
    
    Signed-off-by: DaBeIDS <90916810+DaBeIDS@users.noreply.github.com>
    Signed-off-by: ktj8l89 <tobias.watzel@investmentdataservices.com>

--- data_extractor/code/model_pipeline/model_pipeline/inference_server.py
+++ data_extractor/code/model_pipeline/model_pipeline/inference_server.py
@@ -7,8 +7,6 @@ import collections
 import pathlib
 import shutil
 import traceback
-from s3_communication import S3Communication
-import zipfile
 
 from flask import Flask, Response, request
 from model_pipeline.config_farm_train import InferConfig
@@ -40,26 +38,6 @@ app = Flask(__name__)
 def free_memory():
     gc.collect()
     torch.cuda.empty_cache()
-
-
-def create_directory(directory_name):
-    os.makedirs(directory_name, exist_ok=True)
-    for filename in os.listdir(directory_name):
-        file_path = os.path.join(directory_name, filename)
-        try:
-            if os.path.isfile(file_path):
-                os.unlink(file_path)
-        except Exception as e:
-            print('Failed to delete %s. Reason: %s' % (file_path, e))
-
-
-def zipdir(path, ziph):
-    # ziph is zipfile handle
-    for root, dirs, files in os.walk(path):
-        for file in files:
-            ziph.write(os.path.join(root, file),
-                       os.path.relpath(os.path.join(root, file),
-                                       os.path.join(path, '..')))
 
 
 @app.route("/liveness")
@@ -95,55 +73,19 @@ def run_train_relevance():
         
         model_config.layer_dims = relevance_training_settings["model"]["model_layer_dims"]
         model_config.layer_dims = relevance_training_settings["model"]["model_lm_output_types"]
-        
-        s3_usage = args["s3_usage"]
-        if s3_usage:
-            s3_settings = args["s3_settings"]
-            project_prefix_data = s3_settings['prefix'] + "/" + project_name + '/data'
-            project_prefix_project_models = s3_settings['prefix'] + "/" + project_name + '/models'
-            # init s3 connector
-            s3c_main = S3Communication(
-                s3_endpoint_url=os.getenv(s3_settings['main_bucket']['s3_endpoint']),
-                aws_access_key_id=os.getenv(s3_settings['main_bucket']['s3_access_key']),
-                aws_secret_access_key=os.getenv(s3_settings['main_bucket']['s3_secret_key']),
-                s3_bucket=os.getenv(s3_settings['main_bucket']['s3_bucket_name']),
-            )
-            s3c_interim = S3Communication(
-                s3_endpoint_url=os.getenv(s3_settings['interim_bucket']['s3_endpoint']),
-                aws_access_key_id=os.getenv(s3_settings['interim_bucket']['s3_access_key']),
-                aws_secret_access_key=os.getenv(s3_settings['interim_bucket']['s3_secret_key']),
-                s3_bucket=os.getenv(s3_settings['interim_bucket']['s3_bucket_name']),
-            )
-            create_directory(os.path.dirname(file_config.curated_data))
-            s3c_interim.download_files_in_prefix_to_dir(project_prefix_data + '/interim/ml/curation', 
-                                    os.path.dirname(file_config.curated_data))
-        
-        training_folder = DATA_FOLDER / project_name / 'interim/ml/training'
-        create_directory(training_folder)
-        output_model_folder = os.path.join(str(MODEL_FOLDER), file_config.experiment_name, file_config.experiment_type, 
-                         file_config.data_type, file_config.output_model_name)
-        create_directory(output_model_folder)
-        
+
         if relevance_training_settings["input_model_name"] is not None:
-            base_model_dir = os.path.join(str(MODEL_FOLDER), project_name, "RELEVANCE", "Text")
-            model_dir = os.path.join(base_model_dir, relevance_training_settings["input_model_name"])
+            model_dir = os.path.join(str(MODEL_FOLDER), project_name, 
+                                     "RELEVANCE", "Text", relevance_training_settings["input_model_name"])
             model_config.load_dir = model_dir
             tokenizer_config.pretrained_model_name_or_path = model_dir
             model_config.lang_model = model_dir
             processor_config.load_dir = model_dir
-            if args['s3_usage']:
-                # Download model
-                model_rel_prefix = str(pathlib.Path(s3_settings['prefix']) / project_name / 'models' / 'RELEVANCE' / 'Text')
-                output_model_zip = model_dir + ".zip"
-                s3c_main.download_file_from_s3(output_model_zip, model_inf_prefix, relevance_training_settings["input_model_name"] + ".zip")
-                with zipfile.ZipFile(output_model_zip, 'r') as zip_ref:
-                    zip_ref.extractall(base_model_dir)
-                os.remove(output_model_zip)
         else:
-            model_config.lang_model = relevance_training_settings["base_model"]
+            model_dir = relevance_training_settings["base_model"]
             model_config.load_dir = None
             tokenizer_config.pretrained_model_name_or_path = relevance_training_settings["base_model"]
-            processor_config.load_dir = None
+            processor_config.load_dir = None        
         
         train_config.n_epochs = relevance_training_settings["training"]["n_epochs"]
         train_config.run_hyp_tuning = relevance_training_settings["training"]["run_hyp_tuning"]
@@ -177,24 +119,7 @@ def run_train_relevance():
         name_out = os.path.join(str(MODEL_FOLDER), project_name, "result_rel_" + relevance_training_settings['output_model_name'] + ".json")
         with open(name_out, 'w') as f:
             json.dump(result, f)
-        
-        if s3_usage:
-            train_rel_prefix = os.path.join(project_prefix_project_models, file_config.experiment_type, file_config.data_type)
-            output_model_zip = os.path.join(str(MODEL_FOLDER), file_config.experiment_name, file_config.experiment_type, 
-                 file_config.data_type, file_config.output_model_name + ".zip")
-            with zipfile.ZipFile(output_model_zip, 'w', zipfile.ZIP_DEFLATED) as zipf:
-                zipdir(output_model_folder, zipf)            
-            response = s3c_main.upload_file_to_s3(filepath=output_model_zip,
-                                             s3_prefix=train_rel_prefix,
-                                             s3_key=file_config.output_model_name + ".zip")
-            response_2 = s3c_main.upload_file_to_s3(filepath=name_out,
-                                 s3_prefix=train_rel_prefix,
-                                 s3_key="result_rel_" + file_config.output_model_name + ".json")
-            create_directory(output_model_folder)
-            create_directory(training_folder)
-            create_directory(os.path.dirname(file_config.curated_data))
-            if relevance_training_settings["input_model_name"] is not None:
-                create_directory(model_dir)
+
         t2 = time.time()
     except Exception as e:
         msg = "Error during kpi infer stage\nException:" + str(repr(e) + traceback.format_exc())
@@ -220,59 +145,8 @@ def run_infer_relevance():
     relevance_infer_config.kpi_questions = infer_relevance_settings['kpi_questions']
     relevance_infer_config.sectors = infer_relevance_settings['sectors']
     relevance_infer_config.return_class_probs = infer_relevance_settings['return_class_probs']
-    
-    BASE_DATA_PROJECT_FOLDER =  DATA_FOLDER / project_name
-    BASE_INTERIM_FOLDER = BASE_DATA_PROJECT_FOLDER / 'interim' / 'ml'
-    BASE_OUTPUT_FOLDER = BASE_DATA_PROJECT_FOLDER / 'output' / relevance_infer_config.experiment_type / relevance_infer_config.data_type
-    ANNOTATION_FOLDER = BASE_INTERIM_FOLDER / 'annotations'
-    EXTRACTION_FOLDER = BASE_INTERIM_FOLDER / 'extraction'
 
-    kpi_folder = os.path.join(DATA_FOLDER, project_name, "input", "kpi_mapping")
-
-    output_model_folder = os.path.join(str(MODEL_FOLDER), project_name, relevance_infer_config.experiment_type, 
-                                       relevance_infer_config.data_type)
-
-    create_directory(kpi_folder)
-    create_directory(output_model_folder)
-    create_directory(BASE_OUTPUT_FOLDER)
-    create_directory(ANNOTATION_FOLDER)
-    create_directory(EXTRACTION_FOLDER)
-    
-    s3_usage = args["s3_usage"]
-    if s3_usage:
-        s3_settings = args["s3_settings"]
-        project_prefix_data = s3_settings['prefix'] + "/" + project_name + '/data'
-        project_prefix_project_models = s3_settings['prefix'] + "/" + project_name + '/models'
-        # init s3 connector
-        s3c_main = S3Communication(
-            s3_endpoint_url=os.getenv(s3_settings['main_bucket']['s3_endpoint']),
-            aws_access_key_id=os.getenv(s3_settings['main_bucket']['s3_access_key']),
-            aws_secret_access_key=os.getenv(s3_settings['main_bucket']['s3_secret_key']),
-            s3_bucket=os.getenv(s3_settings['main_bucket']['s3_bucket_name']),
-        )
-        s3c_interim = S3Communication(
-                s3_endpoint_url=os.getenv(s3_settings['interim_bucket']['s3_endpoint']),
-                aws_access_key_id=os.getenv(s3_settings['interim_bucket']['s3_access_key']),
-                aws_secret_access_key=os.getenv(s3_settings['interim_bucket']['s3_secret_key']),
-                s3_bucket=os.getenv(s3_settings['interim_bucket']['s3_bucket_name']),
-        )
-        # Download kpi file
-        s3c_main.download_files_in_prefix_to_dir(project_prefix_data + '/input/kpi_mapping', kpi_folder)
-        # Download model
-        train_rel_prefix = os.path.join(project_prefix_project_models, relevance_infer_config.experiment_type, relevance_infer_config.data_type)
-        output_model_zip = os.path.join(output_model_folder, args["train_relevance"]['output_model_name'] + ".zip")
-        s3c_main.download_file_from_s3(output_model_zip, train_rel_prefix, args["train_relevance"]['output_model_name'] + ".zip")
-        with zipfile.ZipFile(output_model_zip, 'r') as zip_ref:
-            zip_ref.extractall(output_model_folder)
-        os.remove(output_model_zip)
-        # Download extraction files
-        s3c_interim.download_files_in_prefix_to_dir(project_prefix_data + '/interim/ml/extraction', 
-                            EXTRACTION_FOLDER)
-        # Download annotation files        
-        s3c_interim.download_files_in_prefix_to_dir(project_prefix_data + '/interim/ml/annotations', 
-                                    ANNOTATION_FOLDER)
-        
-    shutil.copyfile(os.path.join(kpi_folder, "kpi_mapping.csv"), "/app/code/kpi_mapping.csv")
+    shutil.copyfile(os.path.join(DATA_FOLDER, project_name, "input", "kpi_mapping", "kpi_mapping.csv"), "/kpi_mapping.csv")
     free_memory()
 
     try:
@@ -285,17 +159,7 @@ def run_infer_relevance():
     except Exception as e:
         msg = "Error during kpi infer stage\nException:" + str(repr(e) + traceback.format_exc())
         return Response(msg, status=500)
-    
-    if s3_usage:
-        project_prefix_project_output = pathlib.Path(s3_settings['prefix'] + "/" + project_name + '/output') \
-                / relevance_infer_config.experiment_type / relevance_infer_config.data_type
-        s3c_main.upload_files_in_dir_to_prefix(BASE_OUTPUT_FOLDER,  project_prefix_project_output)
-        create_directory(kpi_folder)
-        create_directory(str(pathlib.Path(output_model_folder) / args["train_relevance"]['output_model_name']))
-        create_directory(BASE_OUTPUT_FOLDER)
-        create_directory(ANNOTATION_FOLDER)
-        create_directory(EXTRACTION_FOLDER)
-    
+
     time_elapsed = str(timedelta(seconds=t2-t1))
     msg = "Inference for the relevance stage finished successfully!\nTime elapsed:{}".format(time_elapsed)
     return Response(msg, status=200)
@@ -317,7 +181,7 @@ def run_train_kpi():
         "relevant_text_path": DATA_FOLDER / project_name / "interim" / "ml" / "text_3434.csv"
     }
     config.seed = kpi_inference_training_settings['seed']
-    
+
     free_memory()
     try:
         t1 = time.time()
@@ -328,43 +192,6 @@ def run_train_kpi():
         tkpi.output_squad_folder = file_config.training_dir
         tkpi.relevant_text_path = os.path.join(file_config.data_dir, project_name, "interim", "ml", "text_3434.csv")
         curation_input = kpi_inference_training_settings["curation"]
-        
-        create_directory(tkpi.annotation_folder)
-        create_directory(str(pathlib.Path(file_config.data_dir) / project_name / "interim" / "ml"))
-        create_directory(tkpi.output_squad_folder)
-        
-        if args['s3_usage']:
-            s3_settings = args["s3_settings"]
-            s3c_main = S3Communication(
-                    s3_endpoint_url=os.getenv(s3_settings['main_bucket']['s3_endpoint']),
-                    aws_access_key_id=os.getenv(s3_settings['main_bucket']['s3_access_key']),
-                    aws_secret_access_key=os.getenv(s3_settings['main_bucket']['s3_secret_key']),
-                    s3_bucket=os.getenv(s3_settings['main_bucket']['s3_bucket_name']),
-                )
-            s3c_interim = S3Communication(
-                    s3_endpoint_url=os.getenv(s3_settings['interim_bucket']['s3_endpoint']),
-                    aws_access_key_id=os.getenv(s3_settings['interim_bucket']['s3_access_key']),
-                    aws_secret_access_key=os.getenv(s3_settings['interim_bucket']['s3_secret_key']),
-                    s3_bucket=os.getenv(s3_settings['interim_bucket']['s3_bucket_name']),
-                )
-            project_prefix_data = pathlib.Path(s3_settings['prefix']) / project_name / 'data'
-            project_prefix_agg_annotations = str(project_prefix_data / 'interim' / 'ml' / 'annotations')
-            
-            # Download aggregated annotations file
-            s3c_interim.download_file_from_s3(filepath=tkpi.agg_annotation,
-                          s3_prefix=project_prefix_agg_annotations,
-                          s3_key='aggregated_annotation.csv')
-            
-            # Download kpi file
-            s3c_main.download_file_from_s3('/app/code/kpi_mapping.csv', s3_prefix=str(project_prefix_data / 'input' / 'kpi_mapping'), s3_key='kpi_mapping.csv')
-            
-            # Download text_3434 file
-            s3c_interim.download_file_from_s3(tkpi.relevant_text_path, s3_prefix=str(project_prefix_data / 'interim' / 'ml'), s3_key='text_3434.csv')
-            
-            # Download extractions
-            s3c_interim.download_files_in_prefix_to_dir(str(project_prefix_data / 'interim' / 'ml' / 'extraction'), 
-                        str(config.TextKPIInferenceCurator_kwargs['extracted_text_json_folder']))
-        
         _, _ = tkpi.curate(curation_input["val_ratio"], curation_input["seed"], curation_input["find_new_answerable"], curation_input["create_unanswerable"])
 
         print('Curation step in train_kpi done.')
@@ -400,19 +227,11 @@ def run_train_kpi():
         processor_config.metric = kpi_inference_training_settings["processor"]["metric"]
         
         if kpi_inference_training_settings["input_model_name"] is not None:
-            base_model_dir = os.path.join(str(MODEL_FOLDER), project_name, "KPI_EXTRACTION", "Text")
-            model_dir = os.path.join(base_model_dir, kpi_inference_training_settings["input_model_name"])
+            model_dir = os.path.join(str(MODEL_FOLDER), project_name, 
+                                     "KPI_EXTRACTION", "Text", kpi_inference_training_settings["input_model_name"])
             model_config.load_dir = model_dir
             tokenizer_config.pretrained_model_name_or_path = model_dir
             model_config.lang_model = model_dir
-            if args['s3_usage']:
-                # Download model
-                model_inf_prefix = str(pathlib.Path(s3_settings['prefix']) / project_name / 'models' / 'KPI_EXTRACTION' / 'Text')
-                output_model_zip = model_dir + ".zip"
-                s3c_main.download_file_from_s3(output_model_zip, model_inf_prefix, kpi_inference_training_settings["input_model_name"] + ".zip")
-                with zipfile.ZipFile(output_model_zip, 'r') as zip_ref:
-                    zip_ref.extractall(base_model_dir)
-                os.remove(output_model_zip)
         else:
             tokenizer_config.pretrained_model_name_or_path = kpi_inference_training_settings["base_model"]
             model_config.load_dir = None
@@ -438,36 +257,7 @@ def run_train_kpi():
         name_out = os.path.join(str(MODEL_FOLDER), project_name, "result_kpi_" + kpi_inference_training_settings['output_model_name'] + ".json")
         with open(name_out, 'w') as f:
             json.dump(result, f)
-        
-        if args['s3_usage']:
-            print('Next we store the files to S3.')
-            train_inf_prefix = str(pathlib.Path(s3_settings['prefix']) / project_name / 'models' / 'KPI_EXTRACTION' / 'Text')
-            output_model_folder = str(pathlib.Path(MODEL_FOLDER) / project_name / "KPI_EXTRACTION" / "Text" / kpi_inference_training_settings['output_model_name'])
-            print('First we zip the model. This can take some time.')
-            with zipfile.ZipFile(output_model_folder + ".zip", 'w', zipfile.ZIP_DEFLATED) as zipf:
-                zipdir(output_model_folder, zipf)            
-            print('Next we upload the model to S3. This can take some time.')
-            response = s3c_main.upload_file_to_s3(filepath=output_model_folder + ".zip",
-                                              s3_prefix=train_inf_prefix,
-                                              s3_key=kpi_inference_training_settings['output_model_name'] + ".zip")
-            os.remove(output_model_folder + ".zip")
-            print('Next we upload the interim training files and the statistics to S3.')
-            response_2 = s3c_main.upload_file_to_s3(filepath=name_out,
-                                  s3_prefix=train_inf_prefix,
-                                  s3_key="result_kpi_" + kpi_inference_training_settings['output_model_name'] + ".json")
-            s3c_interim.upload_files_in_dir_to_prefix(source_dir=str(DATA_FOLDER / project_name / 'interim' / 'ml' / 'training'),
-                                                      s3_prefix=str(pathlib.Path(s3_settings['prefix']) / project_name / 'data' / 'interim' / 'ml' / 'training'))
-            create_directory(output_model_folder)
-            create_directory(output_model_folder)
-            create_directory(os.path.join(str(MODEL_FOLDER), project_name))
-            create_directory(tkpi.annotation_folder)
-            create_directory(str(pathlib.Path(file_config.data_dir) / project_name / "interim" / "ml"))
-            create_directory(str(project_prefix_data / 'input'))
-            create_directory(tkpi.output_squad_folder)
-            create_directory(str(config.TextKPIInferenceCurator_kwargs['extracted_text_json_folder']))
-            if kpi_inference_training_settings["input_model_name"] is not None:
-                create_directory(model_dir)
-        
+            
         t2 = time.time()
 
     except Exception as e:
