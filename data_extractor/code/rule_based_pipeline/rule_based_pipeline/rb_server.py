843557ee140ef8e24468749b7e052f36efdb1638
DaBeIDS <90916810+DaBeIDS@users.noreply.github.com>
2023-07-21 08:52:27 +0200

    Update rb_server.py
    Signed-off-by: DaBeIDS <90916810+DaBeIDS@users.noreply.github.com>

--- data_extractor/code/rule_based_pipeline/rule_based_pipeline/rb_server.py
+++ data_extractor/code/rule_based_pipeline/rule_based_pipeline/rb_server.py
@@ -1,9 +1,23 @@
 import argparse
 import os
+import json
+from s3_communication import S3Communication
 
 from flask import Flask, Response, request
 
 app = Flask(__name__)
+
+
+def create_directory(directory_name):
+    os.makedirs(directory_name, exist_ok=True)
+    for filename in os.listdir(directory_name):
+        file_path = os.path.join(directory_name, filename)
+        try:
+            if os.path.isfile(file_path):
+                os.unlink(file_path)
+        except Exception as e:
+            print('Failed to delete %s. Reason: %s' % (file_path, e))
+
 
 def run_rb_int(raw_pdf_folder, working_folder, output_folder, verbosity):
         cmd = 'python3 /app/code/rule_based_pipeline/rule_based_pipeline/main.py' + \
@@ -14,12 +28,31 @@ def run_rb_int(raw_pdf_folder, working_folder, output_folder, verbosity):
         print("Running command: " + cmd)
         os.system(cmd)
 
-def run_rb(project_name, verbosity):
+
+def run_rb(project_name, verbosity, s3_usage, s3_settings):
     base = r'/app/data/' + project_name 
     raw_pdf_folder = base + r'/interim/pdfs/'
     working_folder = base + r'/interim/rb/work'
     output_folder = base + r'/output/KPI_EXTRACTION/rb'
+    if s3_usage:
+        s3c_main = S3Communication(
+            s3_endpoint_url=os.getenv(s3_settings['main_bucket']['s3_endpoint']),
+            aws_access_key_id=os.getenv(s3_settings['main_bucket']['s3_access_key']),
+            aws_secret_access_key=os.getenv(s3_settings['main_bucket']['s3_secret_key']),
+            s3_bucket=os.getenv(s3_settings['main_bucket']['s3_bucket_name']),
+        )
+        create_directory(base)
+        create_directory(raw_pdf_folder)
+        create_directory(working_folder)
+        create_directory(output_folder)
+        project_prefix = s3_settings['prefix'] + "/" + project_name + '/data'
+        s3c_main.download_files_in_prefix_to_dir(project_prefix + '/input/pdfs/inference', 
+                                    raw_pdf_folder)
     run_rb_int(raw_pdf_folder, working_folder, output_folder, verbosity)
+    if s3_usage:
+        s3c_main.upload_files_in_dir_to_prefix(output_folder, 
+                                  project_prefix + '/output/KPI_EXTRACTION/rb')
+
 
 @app.route("/liveness")
 def liveness():
@@ -28,9 +61,13 @@ def liveness():
 
 @app.route("/run")
 def run():
-    project_name = request.args['project_name']
+    args = json.loads(request.args['payload'])
+    project_name = args['project_name']
+    s3_settings = None
+    if args['s3_usage']:
+        s3_settings = args["s3_settings"]
     verbosity  = int(request.args['verbosity'])
-    run_rb(project_name, verbosity)
+    run_rb(project_name, verbosity, args['s3_usage'], s3_settings)
     return Response(response={}, status=200)
 
 
