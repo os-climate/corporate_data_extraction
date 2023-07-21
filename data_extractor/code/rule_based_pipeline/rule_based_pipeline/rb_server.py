1b3cbd9c3bf6eece0e8498231e0f418fa7415178
DaBeIDS <90916810+DaBeIDS@users.noreply.github.com>
2023-07-21 17:03:17 +0200

    Update rb_server.py
    Signed-off-by: DaBeIDS <90916810+DaBeIDS@users.noreply.github.com>

--- data_extractor/code/rule_based_pipeline/rule_based_pipeline/rb_server.py
+++ data_extractor/code/rule_based_pipeline/rule_based_pipeline/rb_server.py
@@ -79,12 +79,29 @@ def run():
 
 @app.route("/run_xy_ml")
 def run_xy_ml():
-    project_name = request.args['project_name']
+    args = json.loads(request.args['payload'])
+    project_name = args['project_name']
     base = r'/app/data/' + project_name 
     raw_pdf_folder = base + r'/interim/pdfs/'
     working_folder = base + r'/interim/rb/work'
-    output_folder = base + r'/output/KPI_EXTRACTION'
-    csv_path = output_folder + '/' + request.args['csv_name']
+    output_folder = base + r'/output/KPI_EXTRACTION/joined_ml_rb'
+    csv_path = output_folder + '/' + args['csv_name']
+    if args['s3_usage']:
+        s3_settings = args["s3_settings"]
+        s3c_main = S3Communication(
+            s3_endpoint_url=os.getenv(s3_settings['main_bucket']['s3_endpoint']),
+            aws_access_key_id=os.getenv(s3_settings['main_bucket']['s3_access_key']),
+            aws_secret_access_key=os.getenv(s3_settings['main_bucket']['s3_secret_key']),
+            s3_bucket=os.getenv(s3_settings['main_bucket']['s3_bucket_name']),
+        )
+        create_directory(base)
+        create_directory(raw_pdf_folder)
+        create_directory(working_folder)
+        create_directory(output_folder)
+        project_prefix = s3_settings['prefix'] + "/" + project_name + '/data'
+        s3c_main.download_files_in_prefix_to_dir(project_prefix + '/input/pdfs/inference', 
+                            raw_pdf_folder)
+        s3c_main.download_file_from_s3(csv_path, project_prefix + '/output/KPI_EXTRACTION/joined_ml_rb', args['csv_name'])
     cmd = 'python3 /app/code/rule_based_pipeline/rule_based_pipeline/main_find_xy.py' + \
              ' --raw_pdf_folder "' + raw_pdf_folder + '"' +    \
              ' --working_folder "' + working_folder + '"' +    \
@@ -94,6 +111,10 @@ def run_xy_ml():
              ' --verbosity ' + str(request.args['verbosity'])
     print("Running command: " + cmd)
     os.system(cmd)
+    if args['s3_usage']:
+        s3c_main.upload_file_to_s3(filepath=csv_path,
+                      s3_prefix=project_prefix + '/output/KPI_EXTRACTION/joined_ml_rb',
+                      s3_key=args['csv_name'])
     return Response(response={}, status=200)
 
 
