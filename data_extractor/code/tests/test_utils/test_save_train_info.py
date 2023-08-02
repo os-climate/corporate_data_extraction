9c677b25a8c2aafeac358ac8aa11297e790ef5a9
ktj8l89 <tobias.watzel@investmentdataservices.com>
2023-08-02 15:41:53 +0200

    Adding test cases for save_train_info
    Signed-off-by: ktj8l89 <tobias.watzel@investmentdataservices.com>

--- data_extractor/code/tests/test_utils/test_save_train_info.py
+++ data_extractor/code/tests/test_utils/test_save_train_info.py
@@ -4,10 +4,23 @@ import pytest
 from unittest.mock import patch
 import shutil
 import train_on_pdf
+import pickle
 
 @pytest.fixture
 def prerequisites_save_train_info(path_folder_root_testing: Path,
                                   path_folder_temporary: Path) -> Path:
+    """Defines a fixture for creating all prerequisites for save_train_info
+
+    :param path_folder_root_testing: Requesting the root testing folder fixture
+    :type path_folder_root_testing: Path
+    :param path_folder_temporary: Requesting the temporary folder fixture
+    :type path_folder_temporary: Path
+    :return: Returns mocked project_model_dir
+    :rtype: Path
+    :yield: Returns mocked project_model_dir
+    :rtype: Iterator[Path]
+    """
+    
     mock_project_settings = {
         'train_relevance': {
             'output_model_name': 'TEST'
@@ -35,6 +48,11 @@ def prerequisites_save_train_info(path_folder_root_testing: Path,
 
 
 def test_save_train_info_pickle(prerequisites_save_train_info):
+    """Tests if the train info is pickle correctly
+
+    :param prerequisites_save_train_info: _description_
+    :type prerequisites_save_train_info: _type_
+    """
     project_name = 'TEST'
     
     path_project_model_dir = prerequisites_save_train_info
@@ -43,5 +61,31 @@ def test_save_train_info_pickle(prerequisites_save_train_info):
     
     assert len(list(path_project_model_dir.glob('*'))) == 1
     
+
+def test_save_train_info_entries(prerequisites_save_train_info):
     
+    project_name = 'TEST'
+    
+    path_project_model_dir = prerequisites_save_train_info
+    
+    save_train_info(project_name)
+    
+    path_pickled_train_info = list(path_project_model_dir.glob('*'))[0]
+    
+    with open(str(path_pickled_train_info), 'rb') as file:
+        train_info = pickle.load(file)
+    
+    required_keys = [
+        'project_name',
+        'train_settings',
+        'pdfs_used',
+        'annotations',
+        'kpis'
+    ]
+    
+    assert required_keys in train_info.keys()
+    
+
+def test_save_tain_info_return_value(prerequisites_save_train_info):
+    pass
 
