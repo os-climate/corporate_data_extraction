bf1d721672ca340a152bf10f220749dfc66bf085
ktj8l89 <tobias.watzel@investmentdataservices.com>
2023-08-10 18:06:18 +0200

    Ongoing creation of tests
    Signed-off-by: ktj8l89 <tobias.watzel@investmentdataservices.com>

--- data_extractor/code/tests/test_train_on_pdf.py
+++ data_extractor/code/tests/test_train_on_pdf.py
@@ -7,8 +7,92 @@ import requests
 import requests_mock
 import typing
 import config_path
+import typing
+from tests.test_utils.test_running import prerequisite_running
 
 
+@pytest.fixture
+def prerequisite_train_on_pdf_settings_files(path_folder_root_testing) -> tuple[Path, Path]:
+    
+    mocked_project_settings = {
+        's3_usage': False,
+        's3_settings': {},
+        'general': 
+            {
+            'ext_port': 0,
+            'infer_port': 0,
+            'ext_ip': '0.0.0.0',
+            'infer_ip': '0.0.0.0',
+            'delete_interim_files': False
+            },
+        'train_relevance': 
+            {
+                'output_model_name': 'test',
+                'train': False
+            },
+        'train_kpi':
+            {
+                'output_model_name': 'test',
+                'train': False
+            },
+        'extraction':
+            {
+                'use_extractions': False,
+                'store_extractions': False
+            }
+    }
+    
+    mocked_s3_settings = {
+        'prefix': 'test_prefix',
+        'main_bucket': {
+            's3_endpoint': 'S3_END_MAIN',
+            's3_access_key': 'S3_ACCESS_MAIN',
+            's3_secret_key': 'S3_SECRET_MAIN',
+            's3_bucket_name': 'S3_NAME_MAIN'
+        },
+        'interim_bucket': {
+            's3_endpoint': 'S3_END_INTERIM',
+            's3_access_key': 'S3_ACCESS_INTERIM',
+            's3_secret_key': 'S3_SECRET_INTERIM',
+            's3_bucket_name': 'S3_NAME_INTERIM'
+        }
+  
+    }
+    
+    yield mocked_project_settings, mocked_s3_settings
+    
+@pytest.fixture(params=(False, False))
+def prerequisite_train_on_pdf_try_run(
+    request,
+    prerequisite_train_on_pdf_settings_files: tuple[Path, Path],
+    path_folder_root_testing
+    ) -> Mock:
+    
+    path_folder_data = path_folder_root_testing / 'data'
+    path_folder_models = path_folder_root_testing / 'models'
+    project_name = 'TEST'
+    
+    mocked_project_settings, _ = prerequisite_train_on_pdf_settings_files
+    train_relevance, train_kpi = request.param
+    mocked_project_settings['train_relevance']['train'] = train_relevance
+    mocked_project_settings['train_kpi']['train'] = train_kpi
+
+    with (
+        patch('train_on_pdf.argparse.ArgumentParser.parse_args', Mock()) as mocked_argpase,    
+        patch('train_on_pdf.config_path', Mock()) as mocked_config_path,
+        patch('train_on_pdf.yaml', Mock()) as mocked_yaml,
+        patch('train_on_pdf.create_directory', Mock()) as mocked_create_directory
+    ):
+        mocked_argpase.return_value.project_name = project_name
+        mocked_argpase.return_value.s3_usage = 'N'
+        mocked_config_path.DATA_DIR = str(path_folder_data)
+        mocked_config_path.MODEL_DIR = str(path_folder_models)
+        mocked_yaml.safe_load.side_effect = lambda args: mocked_project_settings
+        yield mocked_create_directory
+
+def return_first_argument(*args, **kwargs):
+    return args[0]
+    
 def test_train_on_pdf_check_running(capsys):
     with patch('train_on_pdf.check_running') as mocked_function:
         return_value = train_on_pdf.main()
@@ -90,28 +174,9 @@ def test_train_on_pdf_correct_input_s3_usage(s3_usage):
         assert mocked_input() == s3_usage
 
 
-def return_args(*args, **kwargs):
-    def inner_return(*args, **kwargs):
-        return args[0]
-    return inner_return(*args, **kwargs)
-
-def test_train_on_pdf_s3_usage():
-    mock_s3_settings = {
-        'prefix': 'test_prefix',
-        'main_bucket': {
-            's3_endpoint': 'S3_END_MAIN',
-            's3_access_key': 'S3_ACCESS_MAIN',
-            's3_secret_key': 'S3_SECRET_MAIN',
-            's3_bucket_name': 'S3_NAME_MAIN'
-        },
-        'interim_bucket': {
-            's3_endpoint': 'S3_END_INTERIM',
-            's3_access_key': 'S3_ACCESS_INTERIM',
-            's3_secret_key': 'S3_SECRET_INTERIM',
-            's3_bucket_name': 'S3_NAME_INTERIM'
-        }
-  
-    }
+def test_train_on_pdf_s3_usage(prerequisite_train_on_pdf_settings_files):
+    _, mocked_s3_settings = prerequisite_train_on_pdf_settings_files
+    
     project_name = 'TEST'
     mock_s3_communication = Mock()
     mocked_open = MagicMock()
@@ -119,7 +184,7 @@ def test_train_on_pdf_s3_usage():
     
     try:
         with (
-            patch('train_on_pdf.os.getenv', Mock(side_effect=return_args)),
+            patch('train_on_pdf.os.getenv', Mock(side_effect=return_first_argument)),
             patch('train_on_pdf.argparse.ArgumentParser.parse_args', Mock()) as mocked_argpase,
             patch('train_on_pdf.yaml', mocked_yaml),
             patch('train_on_pdf.open', mocked_open),
@@ -127,30 +192,117 @@ def test_train_on_pdf_s3_usage():
             ):
             mocked_argpase.return_value.project_name = project_name
             mocked_argpase.return_value.s3_usage = 'Y'
-            mocked_yaml.safe_load.side_effect = lambda args: mock_s3_settings
+            mocked_yaml.safe_load.side_effect = lambda args: mocked_s3_settings
             
             train_on_pdf.main()
 
     except:
+        pass
+    finally:
         mocked_open.assert_any_call(f"{config_path.DATA_DIR}/s3_settings.yaml", "r")
 
         mock_s3_communication.assert_any_call(
-            s3_endpoint_url=mock_s3_settings['main_bucket']['s3_endpoint'],
-            aws_access_key_id=mock_s3_settings['main_bucket']['s3_access_key'],
-            aws_secret_access_key=mock_s3_settings['main_bucket']['s3_secret_key'],
-            s3_bucket=mock_s3_settings['main_bucket']['s3_bucket_name']
+            s3_endpoint_url=mocked_s3_settings['main_bucket']['s3_endpoint'],
+            aws_access_key_id=mocked_s3_settings['main_bucket']['s3_access_key'],
+            aws_secret_access_key=mocked_s3_settings['main_bucket']['s3_secret_key'],
+            s3_bucket=mocked_s3_settings['main_bucket']['s3_bucket_name']
         )
         
         mock_s3_communication.assert_any_call(
-            s3_endpoint_url=mock_s3_settings['interim_bucket']['s3_endpoint'],
-            aws_access_key_id=mock_s3_settings['interim_bucket']['s3_access_key'],
-            aws_secret_access_key=mock_s3_settings['interim_bucket']['s3_secret_key'],
-            s3_bucket=mock_s3_settings['interim_bucket']['s3_bucket_name']
+            s3_endpoint_url=mocked_s3_settings['interim_bucket']['s3_endpoint'],
+            aws_access_key_id=mocked_s3_settings['interim_bucket']['s3_access_key'],
+            aws_secret_access_key=mocked_s3_settings['interim_bucket']['s3_secret_key'],
+            s3_bucket=mocked_s3_settings['interim_bucket']['s3_bucket_name']
         )
+        
+        mock_s3_communication.return_value.download_file_from_s3.assert_called_once()
 
 
-def test_train_on_pdf_required_folders():
-    pass
+def test_train_on_pdf_required_settings_file(
+    prerequisite_train_on_pdf_settings_files: tuple[Path, Path],
+    path_folder_root_testing: Path,
+    prerequisite_running):
+    project_settings_expected, _ = prerequisite_train_on_pdf_settings_files
+    
+    try:
+        with (
+            patch('train_on_pdf.yaml', Mock()) as mocked_yaml,
+            patch('train_on_pdf.argparse.ArgumentParser.parse_args', Mock()) as mocked_argpase,
+        ):
+            
+            mocked_argpase.return_value.project_name = 'TEST'
+            mocked_argpase.return_value.s3_usage = 'N'
+            mocked_yaml.safe_load.side_effect = lambda args: project_settings_expected
+        
+            train_on_pdf.main()
+    except:
+        pytest.fail()
 
-def test_train_on_pdf_required_folders_e2e():
-    pass
+@pytest.mark.parametrize(
+    'prerequisite_train_on_pdf_try_run', 
+    [(True, False)], 
+    indirect=True
+) 
+def test_train_on_pdf_folders_default_created(
+    prerequisite_train_on_pdf_try_run: Mock,
+    path_folder_root_testing
+    ):
+    
+    mocked_create_directory = prerequisite_train_on_pdf_try_run
+    paths_folders_default = [
+        r'/interim/ml',
+        r'/interim/pdfs/',
+        r'/interim/ml/annotations/',
+        r'/interim/kpi_mapping/',
+        r'/interim/ml/extraction/',
+        r'/interim/ml/training/',
+        r'/interim/ml/curation/',
+        r'/output/RELEVANCE/Text'
+        ]
+    
+    with (
+        patch('train_on_pdf.link_files', Mock()),
+        patch('train_on_pdf.run_router', side_effect=lambda *args: False)
+        ):
+        train_on_pdf.main()
+        
+        path_folder_root_testing = path_folder_root_testing / 'data'
+        path_folder_root_testing = str(path_folder_root_testing) + '/TEST'
+        
+        for path_current in paths_folders_default:
+            path_folder_current = path_folder_root_testing + path_current
+            mocked_create_directory.assert_any_call(str(path_folder_current))
+    
+
+@pytest.mark.parametrize(
+    'prerequisite_train_on_pdf_try_run', 
+    [(False, True)], 
+    indirect=True
+) 
+@pytest.mark.parametrize(
+    'paths_expected',
+    [
+        r'/RELEVANCE/Text/test',
+        r'/KPI_EXTRACTION/Text/test'
+    ]
+    )
+def test_train_on_pdf_folders_default_created(
+    paths_expected,
+    prerequisite_train_on_pdf_try_run: Mock,
+    path_folder_root_testing
+    ):
+    
+    mocked_create_directory = prerequisite_train_on_pdf_try_run
+    
+    with (
+        patch('train_on_pdf.link_files', Mock()),
+        patch('train_on_pdf.run_router', side_effect=lambda *args: False)
+        ):
+        train_on_pdf.main()
+        
+        path_folder_root_testing = path_folder_root_testing / 'models'
+        path_folder_root_testing = str(path_folder_root_testing) + '/TEST'
+        
+        for path_current in paths_expected:
+            path_folder_current = path_folder_root_testing + path_current
+            mocked_create_directory.assert_any_call(str(path_folder_current))  
