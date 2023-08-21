14d1c4462476dd37d03b2678cbd4792e719d7480
ktj8l89 <tobias.watzel@investmentdataservices.com>
2023-08-21 18:07:57 +0200

    Ongoing test creation
    Signed-off-by: ktj8l89 <tobias.watzel@investmentdataservices.com>

--- data_extractor/code/tests/test_train_on_pdf.py
+++ data_extractor/code/tests/test_train_on_pdf.py
@@ -8,14 +8,17 @@ import requests_mock
 import typing
 import config_path
 import typing
+import sys
+from tests.utils_test import modify_project_settings
 from tests.test_utils.test_running import prerequisite_running
 
 
-@pytest.fixture
-def prerequisite_train_on_pdf_settings_files(
+@pytest.fixture(params=[()])
+def prerequisite_train_on_pdf_try_run(
+    request,
     path_folder_root_testing,
     prerequisite_running
-    ) -> tuple[Path, Path]:
+    ) -> Mock:
     
     mocked_project_settings = {
         's3_usage': False,
@@ -45,56 +48,24 @@ def prerequisite_train_on_pdf_settings_files(
             }
     }
     
-    mocked_s3_settings = {
-        'prefix': 'test_prefix',
-        'main_bucket': {
-            's3_endpoint': 'S3_END_MAIN',
-            's3_access_key': 'S3_ACCESS_MAIN',
-            's3_secret_key': 'S3_SECRET_MAIN',
-            's3_bucket_name': 'S3_NAME_MAIN'
-        },
-        'interim_bucket': {
-            's3_endpoint': 'S3_END_INTERIM',
-            's3_access_key': 'S3_ACCESS_INTERIM',
-            's3_secret_key': 'S3_SECRET_INTERIM',
-            's3_bucket_name': 'S3_NAME_INTERIM'
-        }
-  
-    }
-    
-    yield mocked_project_settings, mocked_s3_settings
-    
-@pytest.fixture(params=[(False, False)])
-def prerequisite_train_on_pdf_try_run(
-    request,
-    prerequisite_train_on_pdf_settings_files: tuple[Path, Path],
-    path_folder_root_testing
-    ) -> Mock:
-    
     path_folder_data = path_folder_root_testing / 'data'
     path_folder_models = path_folder_root_testing / 'models'
     project_name = 'TEST'
     
-    mocked_project_settings, _ = prerequisite_train_on_pdf_settings_files
-    train_relevance, train_kpi = request.param
-    mocked_project_settings['train_relevance']['train'] = train_relevance
-    mocked_project_settings['train_kpi']['train'] = train_kpi
+    mocked_project_settings = modify_project_settings(mocked_project_settings, request.param)
 
     with (
         patch('train_on_pdf.argparse.ArgumentParser.parse_args', Mock()) as mocked_argpase,    
         patch('train_on_pdf.config_path', Mock()) as mocked_config_path,
         patch('train_on_pdf.yaml', Mock()) as mocked_yaml,
-        patch('train_on_pdf.create_directory', Mock()) as mocked_create_directory
+        # patch('train_on_pdf.create_directory', Mock()) as mocked_create_directory
     ):
         mocked_argpase.return_value.project_name = project_name
         mocked_argpase.return_value.s3_usage = 'N'
         mocked_config_path.DATA_DIR = str(path_folder_data)
         mocked_config_path.MODEL_DIR = str(path_folder_models)
         mocked_yaml.safe_load.side_effect = lambda args: mocked_project_settings
-        yield mocked_create_directory
-
-def return_first_argument(*args, **kwargs):
-    return args[0]
+        yield
     
 def test_train_on_pdf_check_running(capsys):
     with patch('train_on_pdf.check_running') as mocked_function:
@@ -108,11 +79,7 @@ def test_train_on_pdf_check_running(capsys):
         assert string_expected in output_cmd
 
 @pytest.mark.parametrize('project_name, output_expected',
-                         [
-                             (None,  None),
-                             ('', None)
-                         ]    
-)
+                         [(None,  None), ('', None)])
 def test_train_on_pdf_wrong_input_project_name(project_name,
                                                output_expected,
                                                capsys):
@@ -176,21 +143,36 @@ def test_train_on_pdf_correct_input_s3_usage(s3_usage):
         train_on_pdf.main()
         assert mocked_input() == s3_usage
 
-def test_train_on_pdf_s3_usage(prerequisite_train_on_pdf_settings_files):
-    _, mocked_s3_settings = prerequisite_train_on_pdf_settings_files
+def test_train_on_pdf_s3_usage(path_folder_root_testing):
+    mocked_s3_settings = {
+        'prefix': 'test_prefix',
+        'main_bucket': {
+            's3_endpoint': 'S3_END_MAIN',
+            's3_access_key': 'S3_ACCESS_MAIN',
+            's3_secret_key': 'S3_SECRET_MAIN',
+            's3_bucket_name': 'S3_NAME_MAIN'
+        },
+        'interim_bucket': {
+            's3_endpoint': 'S3_END_INTERIM',
+            's3_access_key': 'S3_ACCESS_INTERIM',
+            's3_secret_key': 'S3_SECRET_INTERIM',
+            's3_bucket_name': 'S3_NAME_INTERIM'
+        }
+
+    }
     
     project_name = 'TEST'
-    mock_s3_communication = Mock()
+    mocked_s3_communication = Mock()
     mocked_open = MagicMock()
     mocked_yaml = Mock()
     
     try:
         with (
-            patch('train_on_pdf.os.getenv', Mock(side_effect=return_first_argument)),
+            patch('train_on_pdf.os.getenv', Mock(side_effect=lambda *args: args[0])),
             patch('train_on_pdf.argparse.ArgumentParser.parse_args', Mock()) as mocked_argpase,
             patch('train_on_pdf.yaml', mocked_yaml),
             patch('train_on_pdf.open', mocked_open),
-            patch('train_on_pdf.S3Communication', mock_s3_communication)
+            patch('train_on_pdf.S3Communication', mocked_s3_communication)
             ):
             mocked_argpase.return_value.project_name = project_name
             mocked_argpase.return_value.s3_usage = 'Y'
@@ -203,49 +185,27 @@ def test_train_on_pdf_s3_usage(prerequisite_train_on_pdf_settings_files):
     finally:
         mocked_open.assert_any_call(f"{config_path.DATA_DIR}/s3_settings.yaml", "r")
 
-        mock_s3_communication.assert_any_call(
+        mocked_s3_communication.assert_any_call(
             s3_endpoint_url=mocked_s3_settings['main_bucket']['s3_endpoint'],
             aws_access_key_id=mocked_s3_settings['main_bucket']['s3_access_key'],
             aws_secret_access_key=mocked_s3_settings['main_bucket']['s3_secret_key'],
             s3_bucket=mocked_s3_settings['main_bucket']['s3_bucket_name']
         )
         
-        mock_s3_communication.assert_any_call(
+        mocked_s3_communication.assert_any_call(
             s3_endpoint_url=mocked_s3_settings['interim_bucket']['s3_endpoint'],
             aws_access_key_id=mocked_s3_settings['interim_bucket']['s3_access_key'],
             aws_secret_access_key=mocked_s3_settings['interim_bucket']['s3_secret_key'],
             s3_bucket=mocked_s3_settings['interim_bucket']['s3_bucket_name']
         )
         
-        mock_s3_communication.return_value.download_file_from_s3.assert_called_once()
-
-
-def test_train_on_pdf_required_settings_file(
-    prerequisite_train_on_pdf_settings_files: tuple[Path, Path],
-    path_folder_root_testing: Path
-    ):
-    project_settings_expected, _ = prerequisite_train_on_pdf_settings_files
-    
-    try:
-        with (
-            patch('train_on_pdf.yaml', Mock()) as mocked_yaml,
-            patch('train_on_pdf.argparse.ArgumentParser.parse_args', Mock()) as mocked_argpase,
-        ):
-            
-            mocked_argpase.return_value.project_name = 'TEST'
-            mocked_argpase.return_value.s3_usage = 'N'
-            mocked_yaml.safe_load.side_effect = lambda args: project_settings_expected
-        
-            train_on_pdf.main()
-    except:
-        pytest.fail()
+        mocked_s3_communication.return_value.download_file_from_s3.assert_called_once()
 
 def test_train_on_pdf_folders_default_created(
-    prerequisite_train_on_pdf_try_run: Mock,
+    prerequisite_train_on_pdf_try_run,
     path_folder_root_testing
     ):
     
-    mocked_create_directory = prerequisite_train_on_pdf_try_run
     paths_folders_default = [
         r'/interim/ml',
         r'/interim/pdfs/',
@@ -259,7 +219,8 @@ def test_train_on_pdf_folders_default_created(
     
     with (
         patch('train_on_pdf.link_files', Mock()),
-        patch('train_on_pdf.run_router', side_effect=lambda *args: False)
+        patch('train_on_pdf.run_router', side_effect=lambda *args: False),
+        patch('train_on_pdf.create_directory', Mock()) as mocked_create_directory
         ):
         train_on_pdf.main()
         
@@ -273,34 +234,126 @@ def test_train_on_pdf_folders_default_created(
 
 @pytest.mark.parametrize(
     'prerequisite_train_on_pdf_try_run', 
-    [(True, False)], 
+    [('train_relevance', 'train', True)], 
     indirect=True
 ) 
-@pytest.mark.parametrize(
-    'paths_expected',
-    [
-        r'/RELEVANCE/Text/test'
-    ]
-    )
 def test_train_on_pdf_folders_relevance_created(
-    paths_expected,
     prerequisite_train_on_pdf_try_run: Mock,
     path_folder_root_testing
     ):
     
-    mocked_create_directory = prerequisite_train_on_pdf_try_run
-    
     with (
         patch('train_on_pdf.link_files', Mock()),
-        patch('train_on_pdf.run_router', side_effect=lambda *args: False)
+        patch('train_on_pdf.run_router', side_effect=lambda *args: False),
+        patch('train_on_pdf.create_directory', Mock()) as mocked_create_directory
         ):
         train_on_pdf.main()
         
         path_folder_root_testing = path_folder_root_testing / 'models'
         path_folder_root_testing = str(path_folder_root_testing) + '/TEST'
         
-        for path_current in [paths_expected]:
-            path_folder_current = path_folder_root_testing + path_current
-            mocked_create_directory.assert_any_call(str(path_folder_current))  
+        path_folder_expected = path_folder_root_testing + '/RELEVANCE/Text/test'
+        mocked_create_directory.assert_any_call(str(path_folder_expected))
             
-            #TODO Why '/' in paths_expected
+@pytest.mark.parametrize(
+    'prerequisite_train_on_pdf_try_run', 
+    [('train_kpi', 'train', True)], 
+    indirect=True
+) 
+def test_train_on_pdf_folders_kpi_extraction_created(
+    prerequisite_train_on_pdf_try_run: Mock,
+    path_folder_root_testing
+    ):
+    
+    with (
+        patch('train_on_pdf.link_files', Mock()),
+        patch('train_on_pdf.run_router', side_effect=lambda *args: False),
+        patch('train_on_pdf.create_directory', Mock()) as mocked_create_directory
+        ):
+        train_on_pdf.main()
+        
+        path_folder_root_testing = path_folder_root_testing / 'models'
+        path_folder_root_testing = str(path_folder_root_testing) + '/TEST'
+        
+        path_folder_expected = path_folder_root_testing + '/KPI_EXTRACTION/Text/test'
+        mocked_create_directory.assert_any_call(str(path_folder_expected))
+            
+            
+
+@pytest.mark.parametrize(
+    'prerequisite_train_on_pdf_try_run', 
+    [('extraction', 'store_extractions', True)], 
+    indirect=True
+) 
+def test_train_on_pdf_e2e_store_extractions(
+    prerequisite_train_on_pdf_try_run,
+    path_folder_root_testing,
+    capsys):
+    
+    with (
+        patch('train_on_pdf.link_files', Mock()),
+        patch('train_on_pdf.run_router', side_effect=lambda *args: True),
+        patch('train_on_pdf.save_train_info', Mock()) as mocked_save_train_info,
+        patch('train_on_pdf.copy_file_without_overwrite', Mock()) as mocked_copy_files
+        ):
+        
+        mocked_copy_files.return_value = False
+        train_on_pdf.main()
+        
+        path_folder_root = path_folder_root_testing / 'data'
+        path_folder_root_source = str(path_folder_root) + '/TEST/interim/ml/extraction/'
+        path_folder_root_destination = str(path_folder_root) + '/TEST/output/TEXT_EXTRACTION'
+    
+        output_cmd, _ = capsys.readouterr()
+        assert 'Finally we transfer the text extraction to the output folder\n' in output_cmd
+
+        mocked_copy_files.assert_called_with(path_folder_root_source, path_folder_root_destination)
+
+@pytest.mark.parametrize(
+    'prerequisite_train_on_pdf_try_run', 
+    [('general', 'delete_interim_files', True)], 
+    indirect=True
+) 
+def test_train_on_pdf_e2e_delete_interim_files(
+    prerequisite_train_on_pdf_try_run,
+    path_folder_root_testing):
+    
+    paths_folders_default = [
+        r'interim/pdfs/',
+        r'interim/kpi_mapping/',
+        r'interim/ml/annotations/',
+        r'interim/ml/extraction/',
+        r'interim/ml/training/',
+        r'interim/ml/curation/',
+        ]
+    
+    with (
+        patch('train_on_pdf.link_files', Mock()),
+        patch('train_on_pdf.run_router', side_effect=lambda *args: True),
+        patch('train_on_pdf.save_train_info', Mock()) as mocked_save_train_info
+        ):
+        
+        train_on_pdf.main()
+        
+        path_folder_root_testing = path_folder_root_testing / 'data' / 'TEST'
+        
+        for path_current in paths_folders_default:
+            path_folder_current = path_folder_root_testing / path_current
+            assert not any(path_folder_current.iterdir())
+
+def test_train_on_pdf_e2e_save_train_info(
+    prerequisite_train_on_pdf_try_run,
+    capsys
+    ):
+    with (
+        patch('train_on_pdf.link_files', Mock()),
+        patch('train_on_pdf.run_router', side_effect=lambda *args: True),
+        patch('train_on_pdf.save_train_info', Mock()) as mocked_save_train_info
+        ):
+        
+        train_on_pdf.main()
+        
+        mocked_save_train_info.assert_called_once()
+        
+        output_cmd, _ = capsys.readouterr()
+        assert output_cmd == "End-to-end inference complete\n"
--- data_extractor/code/tests/utils_test.py
+++ data_extractor/code/tests/utils_test.py
@@ -1,5 +1,6 @@
 from pathlib import Path
 import pandas as pd
+import typing
 
 
 def project_tests_root() -> Path:
@@ -53,3 +54,19 @@ def create_multiple_xlsx_files(path_folder: Path) -> None:
     """
     for i in range(5):
         create_single_xlsx_file(path_folder, file_name = f'xlsx_file_{i}.xlsx')
+
+def modify_project_settings(project_settings: typing.Dict, 
+                            *args: typing.Tuple[str, str, bool]) -> typing.Dict:
+    """Returns are modified project settings dict based on the input args
+
+    :param project_settings: Project settings
+    :type project_settings: typing.Dict
+    :return: Modified project settings
+    :rtype: typing.Dict
+    """
+    for modifier in args:
+        # check for valid args
+        if len(modifier) == 3:
+            key1, key2, decision = modifier
+            project_settings[key1][key2] = decision
+    return project_settings
