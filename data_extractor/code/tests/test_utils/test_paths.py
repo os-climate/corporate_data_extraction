4b4637fadb934f6534d9cc79c24cf85a5335af9e
ToWaIDS <tobias.watzel@investmentdataservices.com>
2023-10-31 18:37:23 +0100

    Some backup
    Signed-off-by: ToWaIDS <tobias.watzel@investmentdataservices.com>

--- data_extractor/code/tests/test_utils/test_paths.py
+++ data_extractor/code/tests/test_utils/test_paths.py
@@ -1,19 +1,47 @@
 import pytest
 from pathlib import Path
 from utils.paths import ProjectPaths
+from utils.settings import Settings
 
 
 @pytest.fixture
-def path() -> ProjectPaths:
+def paths_project() -> ProjectPaths:
     return ProjectPaths()
 
-def test_set_project_data_folder_and_update_all_paths(path: ProjectPaths):
-    string_path: str = '/test'
-    path_project_folder: Path = Path(string_path)
 
-    path.path_project_data_folder: Path = path_project_folder
 
-    for path_field in path.model_fields.keys():
+def test_set_project_data_folder_and_update_all_paths(paths_project: ProjectPaths):
+    path_project_folder: Path = Path('/test')
+
+    paths_project.path_project_data_folder: Path = path_project_folder
+
+    for path_field in paths_project.model_fields.keys():
         if 'saved_models' not in path_field:
-            path_field: Path = getattr(path, f'{path_field}')
+            path_field: Path = getattr(paths_project, f'{path_field}')
             assert path_field.parts[1] == path_project_folder.name
+
+
+def test_check_that_all_required_paths_exist_in_project_path_object(paths_project: ProjectPaths):
+    list_paths_expected = ['input/pdfs/training', 'input/annotations', 'input/kpi_mapping', 'interim/pdfs', 
+                           'interim/ml/annotations', 'interim/kpi_mapping', 'interim/ml/extraction', 
+                           'interim/ml/curation', 'interim/ml/training', 'RELEVANCE/Text', 'KPI_EXTRACTION/Text', 
+                           'interim/ml', 'output/RELEVANCE/Text']
+
+    for path_field in paths_project.model_fields.keys():
+        path_field_attribute: Path = getattr(paths_project, f'{path_field}')
+        assert str(path_field_attribute) in list_paths_expected
+
+
+def test_set_project_model_folder_and_update_paths(paths_project: ProjectPaths, main_settings: Settings):
+    path_model_folder: Path = Path('/test')
+
+    paths_project.path_project_model_folder: Path = path_model_folder
+
+    for path_field in paths_project.model_fields.keys():
+        if 'saved_models' in path_field:
+            path_field_attribute: Path = getattr(paths_project, f'{path_field}')
+            assert path_field_attribute.parts[1] == path_model_folder.name
+
+
+            # destination_saved_models_relevance = project_model_dir + r'/RELEVANCE/Text'  + r'/' + relevance_training_output_model_name
+    # destination_saved_models_inference = project_model_dir + r'/KPI_EXTRACTION/Text' + r'/' + kpi_inference_training_output_model_name
--- data_extractor/code/utils/paths.py
+++ data_extractor/code/utils/paths.py
@@ -2,28 +2,33 @@ from pathlib import Path
 from utils.helpers import create_tmp_file_path
 from pydantic import Field
 from pydantic_settings import BaseSettings
+from utils.settings import Settings, MainSettings
 
 path_file_running = create_tmp_file_path()
 
 
-class ProjectPaths(BaseSettings):
+class ProjectPaths(BaseSettings):        
     _path_project_data_folder: Path
     _path_project_model_folder: Path
     
-    path_source_pdf: Path = Field(default=Path('input/pdfs/training'))
-    path_source_annotation: Path = Field(default=Path('input/annotations'))
-    path_source_mapping: Path = Field(default=Path('input/kpi_mapping'))
-    path_destination_pdf: Path = Field(default=Path('interim/pdfs'))
-    path_destination_annotation: Path = Field(default=Path('interim/ml/annotations'))
-    path_destination_mapping: Path = Field(default=Path('interim/kpi_mapping'))
-    path_destination_extraction: Path = Field(default=Path('interim/ml/extraction'))
-    path_destination_curation: Path = Field(default=Path('interim/ml/curation'))
-    path_destination_training: Path = Field(default=Path('interim/ml/training'))
-    path_destination_saved_models_relevance: Path = Field(default=Path())
-    path_destination_saved_models_inference: Path = Field(default=Path())
+    path_folder_source_pdf: Path = Field(default=Path('input/pdfs/training'))
+    path_folder_source_annotation: Path = Field(default=Path('input/annotations'))
+    path_folder_source_mapping: Path = Field(default=Path('input/kpi_mapping'))
+    path_folder_destination_pdf: Path = Field(default=Path('interim/pdfs'))
+    path_folder_destination_annotation: Path = Field(default=Path('interim/ml/annotations'))
+    path_folder_destination_mapping: Path = Field(default=Path('interim/kpi_mapping'))
+    path_folder_destination_extraction: Path = Field(default=Path('interim/ml/extraction'))
+    path_folder_destination_curation: Path = Field(default=Path('interim/ml/curation'))
+    path_folder_destination_training: Path = Field(default=Path('interim/ml/training'))
+
+    path_folder_destination_saved_models_relevance: Path = Field(default=Path('RELEVANCE/Text'))
+    path_folder_destination_saved_models_inference: Path = Field(default=Path('KPI_EXTRACTION/Text'))
+    
     path_folder_text_3434: Path = Field(default=Path('interim/ml'))
     path_folder_relevance: Path = Field(default=Path('output/RELEVANCE/Text'))
-    
+
+    def _check_for_project_data_folder_existence(self):
+        pass
     
     @property
     def path_project_data_folder(self) -> Path:
@@ -31,7 +36,7 @@ class ProjectPaths(BaseSettings):
     
     @path_project_data_folder.setter
     def path_project_data_folder(self, path_new_project_data_folder: Path) -> None:
-        list_all_paths_in_model: list[str] = list(ProjectPaths.model_fields.keys())
+        list_all_paths_in_model: list[str] = list(self.model_fields.keys())
 
         for path_field in list_all_paths_in_model:
             if 'saved_models' not in path_field:
@@ -44,7 +49,7 @@ class ProjectPaths(BaseSettings):
     
     @path_project_model_folder.setter
     def path_project_model_folder(self, Path_new_project_model_folder) -> None:
-        list_all_paths_in_model: list[str] = list(ProjectPaths.model_fields.keys())
+        list_all_paths_in_model: list[str] = list(self.model_fields.keys())
 
         for path_field in list_all_paths_in_model:
             if 'saved_models' in path_field:
@@ -52,5 +57,7 @@ class ProjectPaths(BaseSettings):
                 setattr(self, f'{path_field}', Path_new_project_model_folder / path_field_default)
 
 
+    
+    
     # destination_saved_models_relevance = project_model_dir + r'/RELEVANCE/Text'  + r'/' + relevance_training_output_model_name
     # destination_saved_models_inference = project_model_dir + r'/KPI_EXTRACTION/Text' + r'/' + kpi_inference_training_output_model_name
