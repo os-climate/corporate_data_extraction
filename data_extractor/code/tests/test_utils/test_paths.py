7f383c8ed582f37f3cfa348d465b3a609be994f1
ToWaIDS <tobias.watzel@investmentdataservices.com>
2023-11-03 17:34:35 +0100

    Finishing ProjectPaths class
    Signed-off-by: ToWaIDS <tobias.watzel@investmentdataservices.com>

--- data_extractor/code/tests/test_utils/test_paths.py
+++ data_extractor/code/tests/test_utils/test_paths.py
@@ -1,24 +1,15 @@
 import pytest
+from unittest.mock import patch, Mock
 from pathlib import Path
 from utils.paths import ProjectPaths
-from utils.settings import Settings
+from utils.settings import Settings, MainSettings
 
 
 @pytest.fixture
-def paths_project() -> ProjectPaths:
-    return ProjectPaths()
-
-
-
-def test_set_project_data_folder_and_update_all_paths(paths_project: ProjectPaths):
-    path_project_folder: Path = Path('/test')
-
-    paths_project.path_project_data_folder: Path = path_project_folder
-
-    for path_field in paths_project.model_fields.keys():
-        if 'saved_models' not in path_field:
-            path_field: Path = getattr(paths_project, f'{path_field}')
-            assert path_field.parts[1] == path_project_folder.name
+def paths_project(main_settings: Settings) -> ProjectPaths:
+    return ProjectPaths(path_project_data_folder=Path(),
+                        path_project_model_folder=Path(),
+                        main_settings=main_settings)
 
 
 def test_check_that_all_required_paths_exist_in_project_path_object(paths_project: ProjectPaths):
@@ -32,16 +23,72 @@ def test_check_that_all_required_paths_exist_in_project_path_object(paths_projec
         assert str(path_field_attribute) in list_paths_expected
 
 
-def test_set_project_model_folder_and_update_paths(paths_project: ProjectPaths, main_settings: Settings):
-    path_model_folder: Path = Path('/test')
-
-    paths_project.path_project_model_folder: Path = path_model_folder
-
-    for path_field in paths_project.model_fields.keys():
-        if 'saved_models' in path_field:
-            path_field_attribute: Path = getattr(paths_project, f'{path_field}')
-            assert path_field_attribute.parts[1] == path_model_folder.name
+def test_set_project_data_folder(paths_project: ProjectPaths):
+    path_test_folder = Path('/data_folder')
+    paths_project.path_project_data_folder: Path = path_test_folder
+    assert paths_project.path_project_data_folder == path_test_folder
 
 
-            # destination_saved_models_relevance = project_model_dir + r'/RELEVANCE/Text'  + r'/' + relevance_training_output_model_name
-    # destination_saved_models_inference = project_model_dir + r'/KPI_EXTRACTION/Text' + r'/' + kpi_inference_training_output_model_name
+def test_setting_project_data_folder_results_in_update_of_depending_paths(paths_project: ProjectPaths):
+    path_test_folder = Path('/data_folder')
+    paths_project.path_project_data_folder: Path = path_test_folder
+    
+    list_paths_model_fields_filtered = [path_model_field for path_model_field in paths_project.model_fields.keys() 
+                                        if 'saved_models' not in path_model_field]
+    
+    for path_field in list_paths_model_fields_filtered:
+        path_field: Path = getattr(paths_project, f'{path_field}')
+        assert path_field.parts[1] == path_test_folder.name
+
+
+def test_set_project_model_folder(paths_project: ProjectPaths):
+    path_test_folder = Path('/model_folder')
+    paths_project.path_project_model_folder: Path = path_test_folder
+    assert paths_project.path_project_model_folder == path_test_folder
+
+
+def test_setting_path_project_model_folder_results_in_call_of_update_method(paths_project: ProjectPaths):
+    path_test_folder: Path = Path('/model_folder_new')
+    
+    with patch.object(paths_project, '_update_all_paths_depending_on_path_project_model_folder') as mocked_method:
+        paths_project.path_project_model_folder = path_test_folder
+    
+    mocked_method.assert_called_once()
+
+
+def test_set_main_settings(paths_project: ProjectPaths, main_settings: Settings):
+    main_settings_changed: MainSettings = MainSettings()
+    main_settings_changed.general.project_name = 'TEST_NEW'
+
+    paths_project.main_settings = main_settings_changed
+    assert paths_project.main_settings != main_settings
+
+
+def test_setting_main_settings_results_in_call_of_update_method(paths_project: ProjectPaths):
+    main_settings_changed: MainSettings = MainSettings()
+    main_settings_changed.general.project_name = 'TEST_NEW'
+    
+    with patch.object(paths_project, '_update_all_paths_depending_on_path_project_model_folder') as mocked_method:
+        paths_project.main_settings = main_settings_changed
+    
+    mocked_method.assert_called_once()
+
+
+def test_update_all_paths_depending_on_path_project_model_folder(paths_project: ProjectPaths):
+    main_settings_changed: MainSettings = MainSettings()
+    main_settings_changed.general.project_name = 'TEST_NEW'
+    paths_project._main_settings = main_settings_changed
+    path_test_folder: Path = Path('/model_folder_new')
+    paths_project._path_project_model_folder: Path = path_test_folder
+
+    paths_project._update_all_paths_depending_on_path_project_model_folder()
+
+    list_paths_model_fields_filtered: list[str] = [path_model_field for path_model_field in 
+                                                   paths_project.model_fields.keys() if 'saved_models' in path_model_field]
+    list_paths_main_settings: list[Path] = [Path(paths_project.main_settings.train_relevance.output_model_name),
+                                           Path(paths_project.main_settings.train_kpi.output_model_name)]
+    for path_model_field, path_main_settings in zip(list_paths_model_fields_filtered, list_paths_main_settings):
+        path_model_field: Path = getattr(paths_project, f'{path_model_field}')
+        assert path_model_field.parts[1] == path_test_folder.name
+        assert path_model_field.parts[-1] == path_main_settings.name
+    
--- data_extractor/code/utils/paths.py
+++ data_extractor/code/utils/paths.py
@@ -1,15 +1,16 @@
 from pathlib import Path
 from utils.helpers import create_tmp_file_path
-from pydantic import Field
+from pydantic import Field, PrivateAttr
 from pydantic_settings import BaseSettings
 from utils.settings import Settings, MainSettings
 
 path_file_running = create_tmp_file_path()
 
 
-class ProjectPaths(BaseSettings):        
+class ProjectPaths(BaseSettings):
     _path_project_data_folder: Path
     _path_project_model_folder: Path
+    _main_settings: Settings
     
     path_folder_source_pdf: Path = Field(default=Path('input/pdfs/training'))
     path_folder_source_annotation: Path = Field(default=Path('input/annotations'))
@@ -27,8 +28,15 @@ class ProjectPaths(BaseSettings):
     path_folder_text_3434: Path = Field(default=Path('interim/ml'))
     path_folder_relevance: Path = Field(default=Path('output/RELEVANCE/Text'))
 
-    def _check_for_project_data_folder_existence(self):
-        pass
+    def __init__(self, 
+                 path_project_data_folder: Path, 
+                 path_project_model_folder: Path,
+                 main_settings: Settings,
+                 **kwargs):
+        super().__init__(**kwargs)
+        self._path_project_data_folder: Path = path_project_data_folder
+        self._path_project_model_folder: Path = path_project_model_folder
+        self._main_settings: Settings = main_settings
     
     @property
     def path_project_data_folder(self) -> Path:
@@ -36,9 +44,9 @@ class ProjectPaths(BaseSettings):
     
     @path_project_data_folder.setter
     def path_project_data_folder(self, path_new_project_data_folder: Path) -> None:
-        list_all_paths_in_model: list[str] = list(self.model_fields.keys())
+        self._path_project_data_folder: Path = path_new_project_data_folder
 
-        for path_field in list_all_paths_in_model:
+        for path_field in self.model_fields.keys():
             if 'saved_models' not in path_field:
                 path_field_default: Path = self.model_fields[path_field].default
                 setattr(self, f'{path_field}', path_new_project_data_folder / path_field_default)
@@ -48,16 +56,40 @@ class ProjectPaths(BaseSettings):
         return self._path_project_model_folder
     
     @path_project_model_folder.setter
-    def path_project_model_folder(self, Path_new_project_model_folder) -> None:
-        list_all_paths_in_model: list[str] = list(self.model_fields.keys())
+    def path_project_model_folder(self, path_new_project_model_folder: Path) -> None:
+        self._path_project_model_folder: Path = path_new_project_model_folder
+        self._update_all_paths_depending_on_path_project_model_folder()
 
-        for path_field in list_all_paths_in_model:
-            if 'saved_models' in path_field:
-                path_field_default: Path = self.model_fields[path_field].default
-                setattr(self, f'{path_field}', Path_new_project_model_folder / path_field_default)
+    @property
+    def main_settings(self) -> Settings:
+        return self._main_settings
+    
+    @main_settings.setter
+    def main_settings(self, main_settings_new: Settings) -> None:
+        self._main_settings: Settings = main_settings_new
+        self._update_all_paths_depending_on_path_project_model_folder()
 
+    def _update_all_paths_depending_on_path_project_model_folder(self) -> None:
+        list_string_paths_depending_on_path_project_model_folder: list[str] = [
+            'path_folder_destination_saved_models_relevance',
+            'path_folder_destination_saved_models_inference']
+        list_paths_main_settings: list[Path] = [Path(self._main_settings.train_relevance.output_model_name),
+                                                Path(self._main_settings.train_kpi.output_model_name)]
+        
+        for string_model_field, path_main_settings in zip(list_string_paths_depending_on_path_project_model_folder,
+                                                          list_paths_main_settings):
+            setattr(self, string_model_field, self._path_project_model_folder / 
+                    self.model_fields[string_model_field].default / path_main_settings)
+            
 
+_current_project_paths: ProjectPaths | None = None
     
     
-    # destination_saved_models_relevance = project_model_dir + r'/RELEVANCE/Text'  + r'/' + relevance_training_output_model_name
-    # destination_saved_models_inference = project_model_dir + r'/KPI_EXTRACTION/Text' + r'/' + kpi_inference_training_output_model_name
+def get_project_settings() -> ProjectPaths:
+    if _current_project_paths is None:
+        raise TypeError
+    return _current_project_paths
+        
+def setup_project_paths(path_project_data_folder: Path, path_project_model_folder: Path) -> None:
+    global _current_project_paths
+    _current_project_paths = ProjectPaths(path_project_data_folder, path_project_model_folder)
